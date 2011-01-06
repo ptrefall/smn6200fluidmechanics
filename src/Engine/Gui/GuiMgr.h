23837f4df0e4582900ef6088987d08c5fe941592
PTrefall@gmail.com <PTrefall@gmail.com@c628178a-a759-096a-d0f3-7c7507b30227>
2011-01-06 11:53:27 +0000

    Some more work on gui. Now displaying an opengl rendered mouse cursor...

--- src/Engine/Gui/GuiMgr.h
+++ src/Engine/Gui/GuiMgr.h
@@ -26,6 +26,7 @@ namespace Engine
 		void addContext(const CL_String &name, const int &width, const int &height);
 		void addDocument(const CL_String &context_name, const CL_String &path);
 		void addFont(const CL_String &path);
+		void loadCursor(const CL_String &path);
 
 		void inject(const unsigned int &key, bool state);
 		void inject(const CL_Vec2i &mouse_pos);
